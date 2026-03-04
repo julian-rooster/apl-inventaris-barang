3b5bfdbf9e0808a9c2f59f6dd7b6604f47bb9daa
julian-rooster <renrajuliana11@gmail.com>
2026-03-04 23:23:36 +0700

    menambahkan unit test untuk inventory service

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
 
 from services.inventory_service import InventoryService
 
@@ -11,4 +11,27 @@ def test_add_item():
     service.add_item("Laptop", 5)
 
     items = service.get_all_items()
+
     assert len(items) == 1
+
+
+def test_remove_item():
+    service = InventoryService()
+
+    service.add_item("Mouse", 2)
+
+    service.remove_item("Mouse")
+
+    items = service.get_all_items()
+
+    assert len(items) == 0
+
+
+def test_get_items():
+    service = InventoryService()
+
+    service.add_item("Keyboard", 3)
+
+    items = service.get_all_items()
+
+    assert items[0].name == "Keyboard"
