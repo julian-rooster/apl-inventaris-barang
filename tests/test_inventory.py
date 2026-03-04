1486beb6e909c71fe802521addd47336b072849c
aufahiraki <aufahiraki@gmail.com>
2026-03-04 20:32:41 +0700

    menambahkan method add_item, remove_item, dan get_all_items pada inventory service

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -1,10 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
 from services.inventory_service import InventoryService
 
 
 def test_add_item():
+    service = InventoryService()
+    service.add_item("Laptop", 5)
 
-    inventory = InventoryService()
-
-    inventory.add_item("Laptop", 5)
-
-    assert len(inventory.items) == 1
+    items = service.get_all_items()
+    assert len(items) == 1
